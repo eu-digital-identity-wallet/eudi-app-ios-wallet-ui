d6930ebe281925772dfbdb8a7f004fcc2bca726a
Filippos Sakellaropoulos <phisakel@gmail.com>
2023-07-18 00:26:30 +0300

    remove wrong test

--- mdoc-holderTests/mdoc_holderTests.swift
+++ mdoc-holderTests/mdoc_holderTests.swift
@@ -38,39 +38,6 @@ final class mdoc_holderTests: XCTestCase {
         }
     }
 
-	
-	func test_cryptoswift_aes_gcm() throws {
-		let data:[UInt8] = [0,0,0,0]
-		let nonceData:[UInt8] = [0,0,0,0, 0,0,0,0, 0,0,0,1]
-		let key = SymmetricKey(size: .bits256)
-		let nonce = try AES.GCM.Nonce(data: nonceData)
-		let encryptedContent = try AES.GCM.seal(data, using: key, nonce: nonce).combined
-		print(encryptedContent!.toHexString())
-		
-		//let nonceData: Data = nonce.withUnsafeBytes { (p: UnsafeRawBufferPointer) -> Data in  Data(p[0..<p.count]) }
-		let symmKeyData = key.withUnsafeBytes { (p: UnsafeRawBufferPointer) -> Data in  Data(p[0..<p.count]) }
-		print(symmKeyData.count, " ", symmKeyData.toHexString())
-		let gcm = CryptoSwift.GCM(iv: [UInt8](nonceData), mode: .combined)
-		let aes = try CryptoSwift.AES(key: [UInt8](symmKeyData), blockMode: gcm, padding: .noPadding)
-		let encrypted = try aes.encrypt([UInt8](data))
-		print(encrypted.toHexString())
-		print(nonceData.toHexString())
-		
-		guard let sealedBox = try? AES.GCM.SealedBox(combined: nonce + encrypted) else { return }
-		guard let decryptedContent = try? AES.GCM.open(sealedBox, using: key) else { return }
-		XCTAssertEqual(decryptedContent, Data(data))
-	}
  */
 	
-	func test_encode_device_engagement() throws {
-		var de = try XCTUnwrap(DeviceEngagement(data: ScytalesMValidData.deEncoded.bytes))
-		let pkData = ScytalesMValidData.privKey963.dropFirst() // drop initial byte 0x04
-		de.setD(d: [UInt8](pkData.dropFirst(64)))
-		print(ScytalesMValidData.seData.bytes.toHexString())
-		let seCbor = try XCTUnwrap(try CBOR.decode(ScytalesMValidData.seData.bytes))
-		print(seCbor.description)
-		let se = try XCTUnwrap(SessionEstablishment(cbor: seCbor))
-		let sessionEncr = try XCTUnwrap(SessionEncryption(se: se, de: de, handOver: BleTransferMode.QRHandover))
-		print(sessionEncr.transcript.toCBOR(options: CBOROptions()).description)
-	}
 }
