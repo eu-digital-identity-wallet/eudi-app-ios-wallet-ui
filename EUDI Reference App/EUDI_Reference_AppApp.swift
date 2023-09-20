ccb77e70dde989367bd835374fb433ba0a313429
Dimitris Tsiflitzis <dimitris.tsiflitzis@netcompany-intrasoft.com>
2023-09-20 12:54:59 +0300

    [fix] initial meaningful commit

---
+++ EUDI Reference App/EUDI_Reference_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  EUDI_Reference_AppApp.swift
+//  EUDI Reference App
+//
+//  Created by TSIFLITZIS Dimitris on 20/9/23.
+//
+
+import SwiftUI
+
+@main
+struct EUDI_Reference_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
