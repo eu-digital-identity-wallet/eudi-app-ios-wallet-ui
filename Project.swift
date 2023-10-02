6011b98f7620b56a493ef72ad432a3ca8a2a7285
Filippos Sakellaropoulos <phisakel@gmail.com>
2023-10-03 02:41:25 +0300

    added SiopOpenID4VP and CodeScanner packages

--- Project.swift
+++ Project.swift
@@ -1,14 +1,14 @@
 import ProjectDescription
 import ProjectDescriptionHelpers
 let eudiGitUrl = "https://github.com/eu-digital-identity-wallet/"
-let packNames = ["eudi-lib-ios-iso18013-data-model", "eudi-lib-ios-iso18013-security", "eudi-lib-ios-iso18013-data-transfer"]
-let products = ["MdocDataModel18013", "MdocSecurity18013", "MdocDataTransfer18013"]
+let packNames = ["eudi-lib-ios-iso18013-data-transfer", "eudi-lib-ios-siop-openid4vp-swift"]
+let products = ["MdocDataTransfer18013", "SiopOpenID4VP", "CodeScanner"]
 // example call: TUIST_IS_CI=false tuist generate -n
-
+let thirdPartyPackages = ["https://github.com/twostraws/CodeScanner.git"]
 // MARK: - Packages
 var packages: [Package] 
 if Environment.isCI.getBoolean(default: true) {
-       packages = packNames.map { Package.remote(url: "\(eudiGitUrl)\($0)", requirement: .branch("develop")) }
+       packages = packNames.map { Package.remote(url: "\(eudiGitUrl)\($0)", requirement: .branch("main")) } + thirdPartyPackages.map { Package.remote(url: $0, requirement: .branch("main")) }  
 } else {
         // for local development, use local packages
         packages = products.map { Package.local(path: .relativeToManifest("../\($0)")) }
