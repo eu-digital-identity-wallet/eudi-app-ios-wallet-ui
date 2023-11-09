70f00e9ad742d0c867cdf3e2b76128f604fa3ca4
Filippos Sakellaropoulos <phisakel@gmail.com>
2023-11-09 14:55:44 +0200

    update file header

--- AppWalletUI/Sources/RouteDestination.swift
+++ AppWalletUI/Sources/RouteDestination.swift
@@ -13,8 +13,6 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 
-Iso18013HolderDemo
-Created on 25/10/2023 
 */
 
 import Foundation
--- AppWalletUI/Sources/WalletUiApp.swift
+++ AppWalletUI/Sources/WalletUiApp.swift
@@ -1,9 +1,18 @@
-//
-//  mdoc_holderApp.swift
-//  mdoc-holder
-//
-//  Created by ffeli on 21/05/2023.
-//
+/*
+Copyright (c) 2023 European Commission
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+		http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
 
 import SwiftUI
 import EudiWalletKit
@@ -14,6 +23,7 @@ struct WalletUiApp: App {
 	var wallet: EudiWallet
 	init() {
 		wallet = EudiWallet.standard
+		wallet.userAuthenticationRequired = true
 		wallet.trustedReaderCertificates = [Data(name: "scytales_root_ca", ext: "der")!]
 	}
 	
