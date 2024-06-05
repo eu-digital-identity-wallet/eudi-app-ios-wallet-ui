a294a52dd14545727e6ce6678d0f2d3e63fcb18e
Tzouvaras, Stilianos <stylianos.tzouvaras@netcompany.com>
2024-06-05 16:41:43 +0300

    Nw credential offer schema

--- Modules/feature-issuance/Sources/UI/Document/Add/AddDocumentViewModel.swift
+++ Modules/feature-issuance/Sources/UI/Document/Add/AddDocumentViewModel.swift
@@ -147,7 +147,7 @@ final class AddDocumentViewModel<Router: RouterHost>: BaseViewModel<Router, AddD
   }
 
   private func handleDeepLink() {
-    if let deepLink = deepLinkController.getPendingDeepLinkAction(), deepLink.action == .openid4vci {
+    if let deepLink = deepLinkController.getPendingDeepLinkAction(), deepLink.action == .credentialOffer {
       deepLinkController.removeCachedDeepLinkURL()
       router.push(
         with: .credentialOfferRequest(
