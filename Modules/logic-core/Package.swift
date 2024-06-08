40650d7b78fb879337da746dcc9be0519bec7f6e
Tzouvaras, Stilianos <stylianos.tzouvaras@netcompany.com>
2024-06-08 10:52:47 +0300

    Wallet kit update to 0.4.9, fixes for verifier cert chain

--- Modules/logic-core/Package.swift
+++ Modules/logic-core/Package.swift
@@ -14,7 +14,7 @@ let package = Package(
   dependencies: [
     .package(
       url: "https://github.com/eu-digital-identity-wallet/eudi-lib-ios-wallet-kit.git",
-      exact: "0.4.8"
+      exact: "0.4.9"
     ),
     .package(
       name: "logic-resources",
