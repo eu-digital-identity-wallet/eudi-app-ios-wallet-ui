45be9d7dd10138ff37b197e0f44f0cba15bf1317
Tzouvaras, Stilianos <stylianos.tzouvaras@netcompany.com>
2024-07-04 15:28:45 +0300

    New walletkit update

--- Modules/logic-core/Package.swift
+++ Modules/logic-core/Package.swift
@@ -14,7 +14,7 @@ let package = Package(
   dependencies: [
     .package(
       url: "https://github.com/eu-digital-identity-wallet/eudi-lib-ios-wallet-kit.git",
-      exact: "0.5.5"
+      exact: "0.5.6"
     ),
     .package(
       name: "logic-resources",
