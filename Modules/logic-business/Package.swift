3f02db6a9f0956c9832ef305172ec79cd2a928f8
Stylianos Tzouvaras <stylianos.tzouvaras@netcompany.com>
2024-12-03 15:54:31 +0200

    new RQES Sdk Version

--- Modules/logic-business/Package.swift
+++ Modules/logic-business/Package.swift
@@ -43,7 +43,7 @@ let package = Package(
     ),
     .package(
       url: "https://github.com/eu-digital-identity-wallet/eudi-lib-ios-rqes-ui.git",
-      exact: "0.1.0"
+      exact: "0.1.2"
     ),
     .package(name: "logic-test", path: "./logic-test")
   ],
