a75bcf023a5df3e567eb00c8d32133e4a121c38b
Jan-Ingar Tistel <jan-ingar.tistel@digdir.no>
2025-04-03 08:39:30 +0200

    Trust swift macro target for xcode cloud (#2)
    Co-authored-by: Stylianos Tzouvaras <stylianos.tzouvaras@netcompany.com>

---
+++ ci_scripts/ci_post_clone.sh
@@ -0,0 +1,11 @@
+#
+//  ci_post_clone.sh
+//  EudiWallet
+//
+//  Created by Jan-Ingar Tistel on 02/04/2025.
+//
+
+#!/bin/zsh
+
+mkdir -p ~/Library/org.swift.swiftpm/security/
+cp macros.json ~/Library/org.swift.swiftpm/security/
