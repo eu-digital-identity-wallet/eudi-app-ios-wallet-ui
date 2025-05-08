62530b39525b187ea9c75ffecd2d224f21f756d9
DusanNDigDir <dusan.nikolic@digdir.no>
2025-05-08 15:46:18 +0200

    EUW-109 Remove light blue signing box

--- Modules/feature-dashboard/Sources/UI/Dashboard/Tab/Home/HomeTabView.swift
+++ Modules/feature-dashboard/Sources/UI/Dashboard/Tab/Home/HomeTabView.swift
@@ -113,24 +113,6 @@ private func content(
         buttonText: .okButton,
         onDismiss: nil
       )
-
-      HomeCardView(
-        text: LocalizableStringKey.electronicallySignDigitalDocuments,
-        buttonText: LocalizableStringKey.signDocument,
-        illustration: Theme.shared.image.homeContract,
-        learnMoreText: LocalizableStringKey.learnMore,
-        learnMoreAction: {
-          toggleSignDocumentAlert()
-        },
-        action: openSignDocument()
-      )
-      .alertView(
-        isPresented: isSignDocumentAlertShowing,
-        title: .alertSignDocumentsSafely,
-        message: .alertSignDocumentsSafelyMessage,
-        buttonText: .okButton,
-        onDismiss: nil
-      )
     }
     .padding(.horizontal, SPACING_MEDIUM)
   }
