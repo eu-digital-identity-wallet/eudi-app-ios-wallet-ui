9fb18bbc0821182818ac5c9d7b94c7e796521a7a
Dusan Nikolic <dusan.nikolic@digdir.no>
2025-05-20 13:07:01 +0200

    Euw 109 small UI adjustments in app (#9)
    ### Ticket [EUW-109](https://digdir.atlassian.net/browse/EUW-109)
    
    # Description of changes
    1. Removed the light blue signing card.
    2. Removed the "Authenticate" bottom sheet options view from the flow.
    By doing this the flow is changed and the previous online option is
    selected automatically so that the user is sent to the qr-Scanner
    immediately when they press "Authenticate".
    
    ## Type of change
    - UI-update
    
    # How Has This Been Tested?
    - [x] Test suite run successfully
    - [x] Tested through UI interaction. See screenshot video below.
    
    
    # Screenshot video
    
    
    https://github.com/user-attachments/assets/3b24ad26-47dc-45e0-9e8e-4c398d71f151
    
    [EUW-109]:
    https://digdir.atlassian.net/browse/EUW-109?atlOrigin=eyJpIjoiNWRkNTljNzYxNjVmNDY3MDlhMDU5Y2ZhYzA5YTRkZjUiLCJwIjoiZ2l0aHViLWNvbS1KU1cifQ

--- Modules/feature-dashboard/Sources/UI/Dashboard/Tab/Home/HomeTabView.swift
+++ Modules/feature-dashboard/Sources/UI/Dashboard/Tab/Home/HomeTabView.swift
@@ -37,20 +37,9 @@ struct HomeTabView<Router: RouterHost>: View {
       openSignDocument: viewModel.openSignDocument(),
       toggleSignDocumentAlert: viewModel.toggleSignDocumentAlert()
     )
-    .confirmationDialog(
-      .authenticate,
-      isPresented: $viewModel.isAuthenticateModalShowing,
-      titleVisibility: .visible
-    ) {
-      Button(.cancelButton, role: .cancel) {}
-      Button(.inPerson) {
-        viewModel.onShare()
-      }
-      Button(.online) {
+    .onChange(of: viewModel.isAuthenticateModalShowing) { showOnline in
+        guard showOnline else { return }
         viewModel.onShowScanner()
-      }
-    } message: {
-      Text(.authenticateAuthoriseTransactions)
     }
     .confirmationDialog(
       .bleDisabledModalTitle,
@@ -109,24 +98,6 @@ private func content(
         isPresented: isAuthenticateAlertShowing,
         title: .alertAccessOnlineServices,
         message: .alertAccessOnlineServicesMessage,
-        buttonText: .okButton,
-        onDismiss: nil
-      )
-
-      HomeCardView(
-        text: LocalizableStringKey.electronicallySignDigitalDocuments,
-        buttonText: LocalizableStringKey.signDocument,
-        illustration: Theme.shared.image.homeContract,
-        learnMoreText: LocalizableStringKey.learnMore,
-        learnMoreAction: {
-          toggleSignDocumentAlert()
-        },
-        action: openSignDocument()
-      )
-      .alertView(
-        isPresented: isSignDocumentAlertShowing,
-        title: .alertSignDocumentsSafely,
-        message: .alertSignDocumentsSafelyMessage,
         buttonText: .okButton,
         onDismiss: nil
       )
