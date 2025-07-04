a9ecf3fb2c242b7388db62232f1e7c481ae453ab
Stylianos Tzouvaras <stylianos.tzouvaras@netcompany.com>
2025-07-04 21:51:25 +0300

    WalletKit update 0.12.9, dpop fix, new SD-JWT library version

--- Modules/logic-core/Package.swift
+++ Modules/logic-core/Package.swift
@@ -14,7 +14,7 @@ let package = Package(
   dependencies: [
     .package(
       url: "https://github.com/eu-digital-identity-wallet/eudi-lib-ios-wallet-kit.git",
-      exact: "0.12.8"
+      exact: "0.12.9"
     ),
     .package(
       name: "logic-resources",
