5e35ef63e1dae1117730a33df84748bbe69b870a
Stylianos Tzouvaras <stylianos.tzouvaras@netcompany.com>
2025-07-16 12:28:01 +0300

    Dependency updates

--- Modules/logic-core/Package.swift
+++ Modules/logic-core/Package.swift
@@ -14,7 +14,7 @@ let package = Package(
   dependencies: [
     .package(
       url: "https://github.com/eu-digital-identity-wallet/eudi-lib-ios-wallet-kit.git",
-      exact: "0.13.0"
+      exact: "0.13.2"
     ),
     .package(
       name: "logic-resources",
