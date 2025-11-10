e042dc1f0ae9261ce1d903c4719e42d724c2bcc5
Michail Koumtzis <mkoumtzi@netcompany.com>
2025-11-10 15:41:48 +0200

    Updates credential issuer URL

--- Modules/logic-core/Sources/Config/WalletKitConfig.swift
+++ Modules/logic-core/Sources/Config/WalletKitConfig.swift
@@ -98,7 +98,7 @@ struct WalletKitConfigImpl: WalletKitConfig {
       case .DEMO:
         return [
           .init(
-            credentialIssuerURL: "https://issuer.eudiw.dev",
+            credentialIssuerURL: "https://ec.issuer.eudiw.dev",
             client: .public(id: "wallet-dev"),
             authFlowRedirectionURI: URL(string: "eu.europa.ec.euidi://authorization")!,
             usePAR: true,
