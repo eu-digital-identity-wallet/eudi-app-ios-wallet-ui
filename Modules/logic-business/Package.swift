5956b3a928a7a5673dc2022495f66cee5103d02d
Michail Koumtzis <mkoumtzi@netcompany.com>
2026-01-12 11:42:53 +0200

    Updated dependencies

--- Modules/logic-business/Package.swift
+++ Modules/logic-business/Package.swift
@@ -15,7 +15,7 @@ let package = Package(
   dependencies: [
     .package(
       url: "https://github.com/kishikawakatsumi/KeychainAccess.git",
-      from: "3.0.0"
+      from: "4.0.0"
     ),
     .package(
       url: "https://github.com/marmelroy/PhoneNumberKit",
