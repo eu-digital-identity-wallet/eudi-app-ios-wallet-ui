49b69412f0103f9a91e32ff9bf12a558411eb6c1
Dusan Nikolic <dusan.nikolic@digdir.no>
2026-03-03 09:47:59 +0100

    EUW-1249 Revert to clean state pre rebase (#31)
    ## Ticket
    https://digdir.atlassian.net/browse/EUW-1249 
    
    ## Type 
    Reverted the previous library change implementations of the log fix so
    that we start of clean before adding /rebasing EU again.
    
    - [x] Other fix (maintenance or house-keeping)
    
    # How Has This Been Tested?
    
    - [x] Test suite run successfully
    - [x] App runs successfully

--- Modules/logic-core/Sources/Config/WalletKitConfig.swift
+++ Modules/logic-core/Sources/Config/WalletKitConfig.swift
@@ -99,7 +99,7 @@ struct WalletKitConfigImpl: WalletKitConfig {
         return [
           .init(
             credentialIssuerURL: "https://utsteder.test.eidas2sandkasse.net",
-            clientId: "demo-lommebok-test",
+            client: .public(id: "demo-lommebok-test"),
             authFlowRedirectionURI: URL(string: "eu.europa.ec.euidi://authorization")!,
             usePAR: true,
             useDpopIfSupported: true,
@@ -110,7 +110,7 @@ struct WalletKitConfigImpl: WalletKitConfig {
         return [
           .init(
             credentialIssuerURL: "https://utsteder.eidas2sandkasse.dev",
-            clientId: "demo-lommebok-dev",
+            client: .public(id: "demo-lommebok-dev"),
             authFlowRedirectionURI: URL(string: "eu.europa.ec.euidi://authorization")!,
             usePAR: true,
             useDpopIfSupported: true,
