eudi-wallet-no/eudiw-app-ios-wallet-ui

EUW-639 Make the DEV app the new TEST app and keep the DEV functionality in the DEV scheme (#24)
# Ticket
[EUW-596](https://digdir.atlassian.net/browse/EUW-639)

## Type of change
### Bundle-id change for DEMO
- Changed the bundle identifier recognized by the Demo configuration and
ultimately the Demo scheme to the same as used by the current dev app
(i.e the Dev config and dev scheme). Thus, by linking them, I'm making
the Demo config and scheme recognized as being in use by Xcode and Xcode
Cloud. So the new TEST app will be operated and managed through the Demo
config and scheme. That will make it possible to distribute the TEST app
with the Demo scheme builds through TestFlight later.

Pro's:
- We leave the Dev configuration and scheme open for a new DEV app to
make use of later.
- This also changes few things as possible in the project to start with.
And it also means that I should only need to rename DEMO to TEST/Test,
if desired, without messing with the actual content and setup of the
configuration.

### Change issuer ID for the DEV configuration
With this, I've also now made it so that you can debug on DEV and
TEST/DEMO right away as well.

So, when you run the:
- EUDI Wallet Demo scheme, it will run the DEMO configuration which will
eventually be the TEST app. This instance of the app will point to the
TEST environment:
🔗 https://utsteder.test.eidas2sandkasse.net/

- EUDI Wallet Dev scheme (which will eventually become a DEV app when we
create the Dev app), will run the DEV configuration. This instance of
the app will point to the SYSTEST/DEV environment, which is:
🔗 https://utsteder.eidas2sandkasse.dev/

So depending on if you want to hook up to the TEST or SYSTEST/DEV
environment just change the scheme you use here:

<img width="152" height="72" alt="Screenshot 2025-10-10 at 15 32 37"
src="https://github.com/user-attachments/assets/77546d27-0e9a-48ff-9f7b-3d07dc723c98"
/>

# How Has This Been Tested?
The functionality hasn't been tested since it is a bund

Modules/logic-core/Sources/Config/WalletKitConfig.swift
@@ -99,15 +99,15 @@ struct WalletKitConfigImpl: WalletKitConfig {
     case .DEMO:
         .init(
           issuerUrl: "https://utsteder.test.eidas2sandkasse.net",
-          clientId: "wallet-dev",
+          clientId: "demo-lommebok-test",
           redirectUri: URL(string: "eu.europa.ec.euidi://authorization")!,
           usePAR: true,
           useDPoP: true
         )
     case .DEV:
         .init(
-          issuerUrl: "https://utsteder.test.eidas2sandkasse.net",
-          clientId: "demo-lommebok",
+          issuerUrl: "https://utsteder.eidas2sandkasse.dev",
+          clientId: "demo-lommebok-dev",
           redirectUri: URL(string: "eu.europa.ec.euidi://authorization")!,
           usePAR: true,
           useDPoP: true

eu-digital-identity-wallet/eudi-app-ios-wallet-ui

Modules/logic-core/Sources/Config/WalletKitConfig.swift
@@ -101,18 +101,8 @@ struct WalletKitConfigImpl: WalletKitConfig {
       case .DEMO:
         return [
           .init(
-            credentialIssuerURL: "https://issuer.eudiw.dev",
-            clientId: "wallet-dev",
-            keyAttestationsConfig: .init(walletAttestationsProvider: walletKitAttestationProvider),
-            authFlowRedirectionURI: URL(string: "eu.europa.ec.euidi://authorization")!,
-            usePAR: true,
-            useDpopIfSupported: true,
-            cacheIssuerMetadata: true
-          ),
-          .init(
-            credentialIssuerURL: "https://issuer-backend.eudiw.dev",
-            clientId: "wallet-dev",
-            keyAttestationsConfig: .init(walletAttestationsProvider: walletKitAttestationProvider),
+            credentialIssuerURL: "https://utsteder.test.eidas2sandkasse.net",
+            clientId: "demo-lommebok-test",
             authFlowRedirectionURI: URL(string: "eu.europa.ec.euidi://authorization")!,
             usePAR: true,
             useDpopIfSupported: true,
@@ -122,18 +112,8 @@ struct WalletKitConfigImpl: WalletKitConfig {
       case .DEV:
         return [
           .init(
-            credentialIssuerURL: "https://ec.dev.issuer.eudiw.dev",
-            clientId: "wallet-dev",
-            keyAttestationsConfig: .init(walletAttestationsProvider: walletKitAttestationProvider),
-            authFlowRedirectionURI: URL(string: "eu.europa.ec.euidi://authorization")!,
-            usePAR: true,
-            useDpopIfSupported: true,
-            cacheIssuerMetadata: true
-          ),
-          .init(
-            credentialIssuerURL: "https://dev.issuer-backend.eudiw.dev",
-            clientId: "wallet-dev",
-            keyAttestationsConfig: .init(walletAttestationsProvider: walletKitAttestationProvider),
+            credentialIssuerURL: "https://utsteder.eidas2sandkasse.dev",
+            clientId: "demo-lommebok-dev",
             authFlowRedirectionURI: URL(string: "eu.europa.ec.euidi://authorization")!,
             usePAR: true,
             useDpopIfSupported: true,