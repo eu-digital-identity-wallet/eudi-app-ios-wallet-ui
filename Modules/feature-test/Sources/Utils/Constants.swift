827610e9c265e5baf99b9c70eb9502c0bb8b5d1b
Michail Koumtzis <mkoumtzi@netcompany.com>
2026-04-30 12:25:52 +0300

    Update wallet kit and dc api handler

--- Modules/feature-test/Sources/Utils/Constants.swift
+++ Modules/feature-test/Sources/Utils/Constants.swift
@@ -27,7 +27,11 @@ private extension Constants {
   
   static let pkb64 = "pQECIAEhWCBoHIiBQnDRMLUT4yOLqJ1l8mrfNIgrjNnFq4RyZgxSmiJYIGD/Sabu6GejaR4eTiym1JkyjnBNcJ+f59pN+lCEyhVyI1ggC6EPCKyGci++LGWUX3fXpPFW6pYO8pyyKLMKs1qF0jo="
   static let kcSks = KeyChainSecureKeyStorage(serviceName: "name", accessGroup: "Group")
-  static let pk = CoseKeyPrivate(privateKeyId: pkb64, index: 0, secureArea: SoftwareSecureArea.create(storage: kcSks))
+  static var pk: CoseKeyPrivate {
+    get async throws {
+      try await CoseKeyPrivate(privateKeyId: pkb64, index: 0, secureArea: SoftwareSecureArea.create(storage: kcSks), curve: .P256)
+    }
+  }
   static let dr = try! DeviceResponse(data: Constants.sampleCborData.bytes)
 }
 
--- Modules/logic-core/Package.swift
+++ Modules/logic-core/Package.swift
@@ -14,7 +14,7 @@ let package = Package(
   dependencies: [
     .package(
       url: "https://github.com/eu-digital-identity-wallet/eudi-lib-ios-wallet-kit.git",
-      exact: "0.26.0"
+      exact: "0.27.0"
     ),
     .package(
       name: "logic-resources",
