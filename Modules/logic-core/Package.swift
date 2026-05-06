dbac5974d6d40a1cadd11da4826aaa7a7141c834
Michail Koumtzis <mkoumtzi@netcompany.com>
2026-05-06 11:21:57 +0300

    Update Wallet Kit to version 0.28.1

--- Modules/logic-core/Package.swift
+++ Modules/logic-core/Package.swift
@@ -14,7 +14,7 @@ let package = Package(
   dependencies: [
     .package(
       url: "https://github.com/eu-digital-identity-wallet/eudi-lib-ios-wallet-kit.git",
-      exact: "0.28.0"
+      exact: "0.28.1"
     ),
     .package(
       name: "logic-resources",
