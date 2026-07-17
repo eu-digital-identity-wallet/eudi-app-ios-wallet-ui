1a320d598482eeeffb34cf437db1e5230677d102
Michail Koumtzis <mkoumtzi@netcompany.com>
2026-07-17 09:27:28 +0300

    Update WalletKit to 0.37.1 and ISO18013 Data Transfer to 0.23.3

--- Modules/logic-core/Package.swift
+++ Modules/logic-core/Package.swift
@@ -14,7 +14,7 @@ let package = Package(
   dependencies: [
     .package(
       url: "https://github.com/eu-digital-identity-wallet/eudi-lib-ios-wallet-kit.git",
-      exact: "0.36.2"
+      exact: "0.37.1"
     ),
     .package(
       name: "logic-resources",
